c6eb757f942654b04db559ef6c85bf1704f5f080
JoeD <joe.doonan@ef.uk.com>
2018-09-20 10:30:10 +0100

    Section 6 - Lecture 40

---
+++ 013_Switch/src/Switch/Main.java
@@ -0,0 +1,72 @@
+package Switch;
+
+public class Main {
+	public static void main(String[] args) {
+//		int value = 1;
+//		if(value == 1) {
+//			System.out.println("Value was 1");
+//		} else if(value == 2) {
+//			System.out.println("Value was 2");
+//		} else {
+//			System.out.println("Was not 1 or 2");
+//		}
+		
+//		int switchValue = 1;
+//		
+//		switch(switchValue){
+//		
+//			case 1:
+//				System.out.println("Value was 1");
+//				break;
+//				
+//			case 2:
+//				System.out.println("Value was 2");
+//				break;
+//				
+//			case 3: case 4: case 5:
+//				System.out.println("was a 3, or a 4, or a 5");
+//				System.out.println("Actually it was a " + switchValue);
+//				break;
+//				
+//			default:
+//				System.out.println("Was not 1 or 2");
+//				break;
+//		}
+		
+		char charValue = 'J';
+		
+		switch(charValue) {
+			
+			case 'A':
+				System.out.println("A was found");
+				break;
+			case 'B':
+				System.out.println("B was found");
+				break;
+			case 'C': case 'D': case 'E':
+				System.out.println(charValue + " was found");
+				break;
+			default:
+				System.out.println("Could not find A, B, C, D or E");
+				break;
+
+		}
+		
+		String month = "February";
+		
+		switch(month.toLowerCase()) {
+			
+			case "february":
+				System.out.println("Feb");
+				break;
+			case "july":
+				System.out.println("Jul");
+				break;
+			default:
+				System.out.println("Not sure");
+		}
+		
+		
+		
+	}
+}
