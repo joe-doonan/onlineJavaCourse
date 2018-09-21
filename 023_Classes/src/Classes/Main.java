7a9b66da62fb7863944722f85d68e0e51b0d33fa
JoeD <joe.doonan@ef.uk.com>
2018-09-21 13:51:57 +0100

    Section 7 - Lecture 55

---
+++ 023_Classes/src/Classes/Main.java
@@ -0,0 +1,15 @@
+package Classes;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Car porche = new Car();
+		Car holden = new Car();
+		
+		porche.setModel("911");
+		
+		System.out.println("Model is " + porche.getModel());
+	}
+	
+}
